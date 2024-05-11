e70a76ab2d1c21d634dd7ec1ee41281bd562eda0
Junior Reis <gilmaikjunior@gmail.com>
2024-05-10 21:21:39 -0300

    three commit

---
+++ src/Footers.jsx
@@ -0,0 +1,14 @@
+import styles from '../src/Footers.module.css'
+
+export function Footers() {
+ 
+
+    return (
+
+        <footer className={styles}>
+              <p>&copy; Feito por Gil Maik Junior</p>
+        </footer>
+
+    )
+
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {About} from './About.jsx'
 import {Formacao} from './Formacao.jsx'
+import { Footers} from './Footers.jsx'
 import './index.css'
 import { Header } from './Header.jsx'
 
@@ -10,5 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Header />
     <About />
     <Formacao />
+    <Footers />
   </React.StrictMode>,
 )
